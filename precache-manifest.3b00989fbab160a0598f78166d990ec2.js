6115afd8bfb54f28fe3cafd715a55fcc7271bec1
ravi kumar chida <ravi.chanti@gmail.com>
2019-12-20 12:12:05 +0530

    Updates

---
+++ precache-manifest.3b00989fbab160a0598f78166d990ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce05884f1a87e9ac308a964980bb5cc8",
+    "url": "/viswa/index.html"
+  },
+  {
+    "revision": "06e7ef24fa7bd31571ff",
+    "url": "/viswa/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "06e7ef24fa7bd31571ff",
+    "url": "/viswa/static/js/2.188d4065.chunk.js"
+  },
+  {
+    "revision": "11964b02b84e21b18bba57158ccd1e02",
+    "url": "/viswa/static/js/2.188d4065.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bf7d61c8c06da1db846",
+    "url": "/viswa/static/js/main.09299b83.chunk.js"
+  },
+  {
+    "revision": "6e78a4674e43d2823c49",
+    "url": "/viswa/static/js/runtime-main.904a16ae.js"
+  }
+]);
