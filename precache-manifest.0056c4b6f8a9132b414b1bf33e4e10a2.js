349b9b36248cba8d5f2167524e5c764f628a9b5b
ravichida <ravi.chida@gmail.com>
2021-05-22 23:23:57 +0530

    Updates

---
+++ precache-manifest.0056c4b6f8a9132b414b1bf33e4e10a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e182d6bb9e081f81753735e1833917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53263203a2cebab59081",
+    "url": "/static/css/2.56fa3368.chunk.css"
+  },
+  {
+    "revision": "53263203a2cebab59081",
+    "url": "/static/js/2.80b52249.chunk.js"
+  },
+  {
+    "revision": "bab5c9b374b7c96142337f561448ad57",
+    "url": "/static/js/2.80b52249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5934c940c844f0cf4a2d",
+    "url": "/static/js/main.74197933.chunk.js"
+  },
+  {
+    "revision": "685cabe4fab69b6bca06",
+    "url": "/static/js/runtime-main.d10c8679.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/viswa/precache-manifest.d74a86e129800d940850a2d260464a24.js"
+  "/precache-manifest.0056c4b6f8a9132b414b1bf33e4e10a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viswa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
