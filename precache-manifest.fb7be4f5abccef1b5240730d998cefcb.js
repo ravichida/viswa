7e3ada0c27e6052c6c5bb4e40391c8294483e735
ravichida <ravi.chida@gmail.com>
2021-05-24 20:13:09 +0530

    Updates

---
+++ precache-manifest.fb7be4f5abccef1b5240730d998cefcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedacc4d665e22237d1cf80ce4307c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5d491f7978fa6bf5df1",
+    "url": "/static/css/2.bed90eae.chunk.css"
+  },
+  {
+    "revision": "f5d491f7978fa6bf5df1",
+    "url": "/static/js/2.f8df19a2.chunk.js"
+  },
+  {
+    "revision": "b16ea966bb7d8cbdf2e68d1c0292e753",
+    "url": "/static/js/2.f8df19a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e100449c407298c9c65f",
+    "url": "/static/js/main.637130ab.chunk.js"
+  },
+  {
+    "revision": "9811d461861a94d6021d",
+    "url": "/static/js/runtime-main.9c417394.js"
+  }
+]);
