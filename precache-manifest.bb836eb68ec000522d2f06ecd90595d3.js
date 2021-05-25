81232835cae6c2fe49927639d9545a582b41b519
ravichida <ravi.chida@gmail.com>
2021-05-25 13:54:32 +0530

    Updates

---
+++ precache-manifest.bb836eb68ec000522d2f06ecd90595d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07d427a1ef42cbbd864acb0b1fe965b",
+    "url": "/viswa/index.html"
+  },
+  {
+    "revision": "1249c1aca90def5f4cba",
+    "url": "/viswa/static/css/2.11390ac7.chunk.css"
+  },
+  {
+    "revision": "85de7fe9c161a3e9227b",
+    "url": "/viswa/static/css/main.2ae5d8fa.chunk.css"
+  },
+  {
+    "revision": "1249c1aca90def5f4cba",
+    "url": "/viswa/static/js/2.2520f1a6.chunk.js"
+  },
+  {
+    "revision": "abc97a6169bf0031d9b3accac1152391",
+    "url": "/viswa/static/js/2.2520f1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85de7fe9c161a3e9227b",
+    "url": "/viswa/static/js/main.85d717e1.chunk.js"
+  },
+  {
+    "revision": "db9a0d4f54a667f4464a",
+    "url": "/viswa/static/js/runtime-main.094dd81d.js"
+  }
+]);
