f592fe2f1759f7d38a5e2bdfafd24b3537127498
ravichida <ravi.chida@gmail.com>
2021-05-29 17:28:33 +0530

    login working version

--- src/menu.js
+++ src/menu.js
@@ -1,58 +1,53 @@
 import React from 'react';
-import {Link,} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useHistory} from "react-router-dom";
 
 class Menu extends React.Component {
-    Links = [
-        {name: <i className='fas fa-home'> Home</i>, path: "/home", id: 1},
-        {name: <i className='fas fa-th-large'> Orders</i>, path: "/orders", id: 2},
-        // {name: <i className='fas fa-info-circle'>  Details</i> , path: "/od", id: 3},
-        {name: <i className='fas fa-search'> Search</i>, path: "/search", id: 3},
-        {name: <i className='fas fa-plus'> Add</i>, path: "/add", id: 4},
-        // {name: <i className='fas fa-home'> Log Out</i>, path: "/", id: 5}
+    Urls = [
+        {name: <i className='fas fa-home'> Login</i>, path: "/", id: 1},
+        {name: <i className='fas fa-home'> Home</i>, path: "home", id: 2},
+        {name: <i className='fas fa-th'> Orders</i>, path: "/orders", id: 3},
+        {name: <i className='fas fa-search'> Search</i>, path: "/search", id: 4},
+        {name: <i className='fas fa-plus'> Add</i>, path: "/add", id: 5}
     ]
 
     constructor(props) {
         super(props);
-        this.state = {
-            activeId: 1,
-            locPath: ""
-        };
+
+        this.state = {activeId: 1};
     }
 
-    handleClick(event, link) {
-        event.preventDefault();
-        console.log("id", link.id);
-        console.log("path", link.path);
-        this.setState({
-            activeId: link.id,
-            locPath: link.path
-        })
-        console.log("state id", this.state.activeId);
-        console.log("state path", this.state.locPath);
+    handleClick(event, id) {
+        // event.preventDefault();
+        // console.log("id", id);
+        this.setState({activeId: id})
     }
 
     componentDidMount(links) {
+        // console.log("menu.js", window.location.pathname);
         let location = window.location.pathname;
-    }
-
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
+        this.Urls.map(link => {
+            if (location === link.path) {
+                this.setState({activeId: link.id});
+                // console.log("active id", link.path, link.id);
+            }
+        });
     }
 
     render() {
         return (
             <div>
                 <nav className="nav nav-pills nav-justified">
+
                     {
-                        this.Links.map(link =>
+                        this.Urls.map(url =>
                             <Link
-                                key={link.id}
-                                to={link.path}
-                                className={this.state.activeId === link.id ? "nav-link nav-item btn-success menublock active" : "nav-link nav-item btn-success menublock"}
-                                onClick={(event) => this.handleClick(event, link)}
+                                key={url.id}
+                                to={url.path}
+                                className={this.state.activeId === url.id ? "nav-link nav-item btn-success menublock active" : "nav-link nav-item btn-success menublock"}
+                                onClick={(event) => this.handleClick(event, url.id)}
                             >
-                                {link.name}
+                                {url.name}
                             </Link>
                         )
                     }
---
+++ src/menu_old.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Link,} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+
+class Menu extends React.Component {
+    Links = [
+        {name: <i className='fas fa-home'> Home</i>, path: "/home", id: 1},
+        {name: <i className='fas fa-th-large'> Orders</i>, path: "/orders", id: 2},
+        // {name: <i className='fas fa-info-circle'>  Details</i> , path: "/od", id: 3},
+        {name: <i className='fas fa-search'> Search</i>, path: "/search", id: 3},
+        {name: <i className='fas fa-plus'> Add</i>, path: "/add", id: 4},
+        // {name: <i className='fas fa-home'> Log Out</i>, path: "/", id: 5}
+    ]
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeId: 1,
+            locPath: ""
+        };
+    }
+
+    handleClick(event, link) {
+        event.preventDefault();
+        console.log("id", link.id);
+        console.log("path", link.path);
+        this.setState({
+            activeId: link.id,
+            locPath: link.path
+        })
+        console.log("state id", this.state.activeId);
+        console.log("state path", this.state.locPath);
+        setTimeout(() => {
+            window.location.href = window.location.href + this.state.locPath;
+        }, 300)
+    }
+
+    componentDidMount(links) {
+        // let location = window.location.pathname;
+        let location = window.location.pathname;
+        /*this.Links.map(link => {
+            if (location === link.path) {
+                this.setState({activeId: link.id});
+                // console.log("active id", link.path, link.id);
+            }
+        });*/
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="nav nav-pills nav-justified">
+                    {
+                        this.Links.map(link =>
+                            <Link
+                                key={link.id}
+                                to={link.path}
+                                className={this.state.activeId === link.id ? "nav-link nav-item btn-success menublock active" : "nav-link nav-item btn-success menublock"}
+                                onClick={(event) => this.handleClick(event, link)}
+                            >
+                                {link.name}
+                            </Link>
+                        )
+                    }
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Menu
